47339b6338813ea8febe8980a7e6695424cc537a
NIH1L <916721371@qq.com>
2025-02-19 22:24:49 +0800

    Update main.cpp

--- CompilerInfoTest/main.cpp
+++ CompilerInfoTest/main.cpp
@@ -2,7 +2,7 @@
 #include "CompilerInfo.h"  // 引入 DLL 的头文件
 
 int main() {
-    const char* exePath = "C:\\Users\\admin\\Downloads\\HPDesignjetZ6100PhotoPrinterSeriesPS3.exe";  // 你要检测的可执行文件路径（确保 a.exe 在同一目录下）
+    const char* exePath = "C:\\Users\\admin\\Downloads\\HPDesignjetZ6100PhotoPrinterSeriesPS3.exe";  // 要检测的可执行文件路径
 
     const char* compilerInfo = GetCompilerInfo(exePath);
     std::cout << "编译器信息: " << compilerInfo << std::endl;
