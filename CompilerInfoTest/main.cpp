388cc31e366dfbdcfb850aad42021615240a6900
NIH1L <jmm960410>
2025-03-10 00:18:00 +0800

    1

--- CompilerInfoTest/main.cpp
+++ CompilerInfoTest/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     //const char* exePath = "C:\\Users\\admin\\Downloads\\HPDesignjetZ6100PhotoPrinterSeriesPS3.exe";  // 要检测的可执行文件路径
-    const char* jsonInput = R"({"exePath": "C:\\Users\\91672\\Downloads\\HPDesignjetZ6100PhotoPrinterSeriesPS3.exe","rulesPath": "D:\\files\\codes\\C\\NIH1L\\CompilerInfoSolution\\rules.json"})";// 要检测的可执行文件路径
+    const char* jsonInput = R"({"exePath": "C:\\Users\\admin\\Downloads\\HPDesignjetZ6100PhotoPrinterSeriesPS3.exe","rulesPath": "D:\\files\\codes\\C\\NIH1L\\CompilerInfoSolution\\rules.json"})";// 要检测的可执行文件路径
 
     //const char* compilerInfo = GetCompilerInfo(exePath);
     const char* compilerInfo = GetCompilerInfoJson(jsonInput);
